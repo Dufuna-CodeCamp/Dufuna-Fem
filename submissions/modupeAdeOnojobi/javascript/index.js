a1b0b1cbd034fc6db112f5b47bf8dd7ac820178a
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2020-12-02 09:05:51 +0100

    Complete the second javaScript task

--- submissions/modupeAdeOnojobi/javascript/index.js
+++ submissions/modupeAdeOnojobi/javascript/index.js
@@ -13,3 +13,14 @@ for (let i = 1; i <= x; i++) {
         console.log(i);
     }
 }
+
+// Second Task
+let productList = [
+    { productName: 'Victoria Sneaker', imageUrl: 'sneaker2.jpg', productPrice: '₦19,900' },
+    { productName: 'Adidas Grand Court Sneaker', imageUrl: 'sneakers.jpg', productPrice: '₦20,000' },
+    { productName: 'Victoria Totem 1491', imageUrl: 'sneaker3.jpg', productPrice: '₦25,000' }
+]
+
+productList.forEach((product) => {
+    console.log(`${product.productName} with ${product.imageUrl} is sold for ${product.productPrice}`);
+})
