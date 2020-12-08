739aa2e600290a0af7dc855aa0f821e16e1647ed
Ruby Adeniregun <rubyadeniregun@gmail.com>
2020-12-08 23:44:58 +0100

    firstjavatask

---
+++ javaScript/conditionals.js
@@ -0,0 +1,19 @@
+var str = "";
+    i = 0
+
+do {
+    i++;
+    str +=i;
+}
+while (i < 116)
+
+console.log(str);
+for (var str = ""; str < 116; str+=i) {
+if (str / 3 === 0) { 
+    console.log("software");
+    } if (str / 5 === 0) {
+    console.log("developer");
+    } if (str / 3 === 0 && str / 5 === 0); {
+    console.log("SoftwareDeveloper")
+    }
+}
