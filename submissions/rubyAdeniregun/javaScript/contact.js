8b61c48722cca6b52ab9a02d8f9a81fe81bc8742
Ruby Adeniregun <rubyadeniregun@gmail.com>
2020-12-14 01:45:53 +0100

    first-task

---
+++ submissions/rubyAdeniregun/javaScript/contact.js
@@ -0,0 +1,69 @@
+let submitForm = document.getElementById('validateForm'),
+    fullName = document.getElementById('fullName'),
+    email = document.getElementById('email'),
+    subject = document.getElementById('subject'),
+    comments = document.getElementById('comments');
+
+function required(field, event) {
+    if (fullName.value == '') {
+        fullName.nextElementSibling.innerHTML = "Please enter your Name";
+        event.preventDefault();
+    
+    if (subject.value == '')
+        subject.nextElementSibling.innerHTML = "Please enter a message subject";
+        event.preventDefault();
+
+    if (comments.value == '')
+        comments.nextElementSibling.innerHTML = "Please enter your message";
+        event.preventDefault();
+        return false;
+    
+    }   else { 
+        field.nextElementSibling.innerHTML = "";
+        return true;
+    }
+}
+
+function emailfieldvalidation(event) {
+    let emailRegex =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!email.value.match(emailRegex)) {
+        email.nextElementSibling.innerHTML = "please enter a valid email";
+        event.preventDefault();
+
+    if (email.value == '')
+        email.nextElementSibling.innerHTML = "Please enter your Email Address";
+        event.preventDefault();
+        return false;
+    }   
+    
+    else {
+        email.nextElementSibling.innerHTML = "";
+        return true;
+    }
+}
+
+function nameValidation(event) {
+    let lettersRegex =   /^[A-Za-z]+$/;;
+    if (!fullName.value.match(lettersRegex)) {
+        fullName.nextElementSibling.innerHTML = "Please enter your name";
+        event.preventDefault();
+    }
+
+    else {
+        fullName.nextElementSibling.innerHTML = "";
+        return true;
+    }
+
+}
+
+function validateForm(e){
+    required(fullName, e);
+    required(email, e);
+    required(subject, e);
+    required(comments, e);
+    emailfieldvalidation(e);
+    nameValidation(e);
+    return true;  
+}
+
+submitForm.addEventListener("submit", validateForm);
