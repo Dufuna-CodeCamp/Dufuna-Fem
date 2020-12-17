3f7b7133f02c9a47bf92f042e2ae4da27bb7f399
Ruby Adeniregun <rubyadeniregun@gmail.com>
2020-12-17 12:26:19 +0100

    price-page

--- submissions/rubyAdeniregun/DOM/product.js
+++ submissions/rubyAdeniregun/DOM/product.js
@@ -26,3 +26,52 @@ let submitForm1 = document.getElementById('verifyForm'),
     }
     
     submitForm1.addEventListener("submit", verifyForm);
+
+//validate "buynow" button
+let submitForm = document.getElementById('validateForm'),
+    item = document.getElementById("quantityBox")
+    btn = document.getElementById("buyNow");
+
+
+function qtyValidation(event) {
+    let positiveRegex =  /^\d+$/;
+    if (!item.value.match(positiveRegex)) {
+        item.nextElementSibling.innerHTML = "Please enter a valid quantity";
+        btn.disabled = true;
+        event.preventDefault();
+        console.dir(btn)
+        
+
+    if (item.value == '')
+        item.nextElementSibling.innerHTML = "Please enter the quantity";
+        event.preventDefault();
+        return false;
+    }   
+    
+    else {
+        item.nextElementSibling.innerHTML = "";
+        btn.enabled = false;
+        return true;
+    }
+}
+
+function validateForm(e){
+    qtyValidation(e);
+    return true;  
+}
+
+submitForm.addEventListener("submit", validateForm);
+
+//Product Price calculation
+let price = document.getElementById("price");
+
+pricing = 2500 * item.value;
+function calculate(pricing, value) {
+    if (item.value == '>=1') {
+        price.innerHTML = "pricing";
+
+}   else { 
+    price.innerHTML = "2,500";
+    return true;
+    }
+}
