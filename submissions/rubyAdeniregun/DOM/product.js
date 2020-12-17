8d4d0e3f5a082f76f607929ad6f53763deb4f91f
Ruby Adeniregun <rubyadeniregun@gmail.com>
2020-12-17 23:33:54 +0100

    finished task

--- submissions/rubyAdeniregun/DOM/product.js
+++ submissions/rubyAdeniregun/DOM/product.js
@@ -24,26 +24,37 @@ let submitForm1 = document.getElementById('verifyForm'),
         footeremailvalidation(e);
         return true;  
     }
-    
-    submitForm1.addEventListener("submit", verifyForm);
+
+submitForm1.addEventListener("submit", verifyForm);
 
 
-let submitForm = document.getElementById('validateForm'),
-    item = document.getElementById("quantityBox")
+//EXAMPLE
+
+let item = document.getElementById("quantityBox")
     button = document.getElementById("buyNow");
     shippingCost = document.getElementById("shippingCost");
     cost = document.getElementById("totalCost");
     price = document.getElementById("price");
-let priceValue = price.innerHTML;
-let costValue = cost.innerHTML;
-let shippingValue = shippingCost.innerHTML;
 
-
-//Product quantity calculation
-
+//initial value
+let priceValue = price.innerHTML,
+    costValue = cost.innerHTML;
+//RegEx
+const Regex = /^[1-9]/;
 function calculateQuantity() {
+    if (!item.value.match(Regex)) {
+        button.disable = true
+        button.style = "background-color: grey"
+        item.nextElementSibling.innerHTML = "⚠️Please enter a valid quantity";
+    } else {
+        button.disabled = false;
+        item.nextElementSibling.innerHTML = "";
+    }
+}
+function calculatePrice() {
     if (item.value >= 1) {
-        calculatePrice();
+        let perPrice = "$ " + (2500 * item.value)
+        price.innerHTML = perPrice
     } else if (item.value === "") {
         price.innerHTML = priceValue
         cost.innerHTML = costValue
@@ -52,111 +63,40 @@ function calculateQuantity() {
         return false
     }
 }
-
-//Product Price calculation
-function calculatePrice() {
-if (item.value >= 1) {
-    let perPrice = priceValue * item.value;
-    price.innerHTML = perPrice;
-    
-}   else if (item.value === "") {
-    price.innerHTML = priceValue;
-    cost.innerHTML = costValue;
-
-}
-    else {
-    return false;
-    }
-}
-
-function calculatePrice() {
-if (item.value >= 1) {
-    let perPrice = priceValue * item.value;
-    price.innerHTML = perPrice;
-    
-}   else if (item.value === "") {
-    price.innerHTML = priceValue;
-    cost.innerHTML = costValue;
-
-}
-    else {
-    return false;
-    }
-}
-
-//shipping Price calculation
-function calculateShipping() {
-    let perPrice = priceValue * item.value
-        if (perPrice < 1000) {
+function calculateShippingPrice() {
+    let perPrice = (2500 * item.value)
+    if (perPrice < 1000) {
         let shippingRange = 100;
-        shippingCost.innerHTML = shippingRange;    
-    }
-
-    else if ((perPrice >= 1000) && (perPrice <= 20000)) {
+        shippingCost.innerHTML = shippingRange;
+    } else if ((perPrice >= 1000) && (perPrice <= 20000)) {
         shippingRange = 0.15 * perPrice;
         shippingCost.innerHTML = shippingRange;
-
-    }
-    else if ((perPrice >= 20000) && (perPrice <= 100000)) {
+    } else if ((perPrice >= 20000) && (perPrice <= 100000)) {
         shippingRange = 0.2 * perPrice;
         shippingCost.innerHTML = shippingRange;
-
-    }
-
-    else if (perPrice >= 100000) {
+    } else if (perPrice >= 100000) {
         shippingRange = 25000;
         shippingCost.innerHTML = shippingRange;
+    } else {
+        return false
     }
-    
-    else {
-    return false;
-        }
 }
-
-// Total Price calculation
-function totalPrice() {
-if (item.value >= 1) {
-    let perPrice = priceValue * item.value,
+function calculateTotalPrice() {
+    if (item.value >= 1) {
+        let perPrice = (2500 * item.value);
             shipCost = Number(shippingCost.innerHTML);
-        cost.innerHTML = perPrice + shipCost;
+        cost.innerHTML = "$ " + (perPrice + shipCost);
     } else {
         return false;
     }
 }
 
 function calculate() {
+    calculateQuantity();
     calculatePrice();
-    calculateShipping();
-    totalPrice();
+    calculateShippingPrice();
+    calculateTotalPrice();
+
 }
 
 item.addEventListener("input", calculate);
-    
-
-function calculateQuantity(event) {
-    let positiveRegex =  /^[1-9]/;
-    if (!item.value.match(positiveRegex)) {
-        button.disable = true
-        button.style = "background-color: grey"
-        item.nextElementSibling.innerHTML = "Please enter a valid quantity";
-        event.preventDefault();        
-
-    if (item.value == '')
-        item.nextElementSibling.innerHTML = "Please enter the quantity";
-        event.preventDefault();
-        return false;
-    }   
-    
-    else {
-        item.nextElementSibling.innerHTML = "";
-        button.disabled = false;
-        return true;
-    }
-}
-
-function validateForm(e){
-    calculateQuantity(e);
-    return true;  
-}
-
-submitForm.addEventListener("submit", validateForm);
