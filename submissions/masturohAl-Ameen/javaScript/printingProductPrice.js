db9ac2bc7d3b49357eed9cfabc8c286d74988d0d
Masturoh Al-Ameen <misturaeneeolah@gmail.com>
2020-12-21 07:17:26 +0100

    [198] - Printing Product Price with JavaScript - Fullstack

--- submissions/masturohAl-Ameen/javaScript/printingProductPrice.js
+++ submissions/masturohAl-Ameen/javaScript/printingProductPrice.js
@@ -4,13 +4,11 @@ var productList = [
         imageURL: "images/shorts.jpg",
         productPrice: "#20,000",
     },
-
     {
         productName: "Nightwear",
         imageURL: "images/nightwear.jpg",
         productPrice: "#10,000",
     },
-
     {
         productName: "Pajamas",
         imageURL: "images/pajamas.jpg",
