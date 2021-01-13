068c39285bce8dbad2e94bf1b2da492dd9662ec5
Ruby Adeniregun <rubyadeniregun@gmail.com>
2021-01-13 17:24:58 +0100

    updated

--- submissions/rubyAdeniregun/Database/Queries.sql
+++ submissions/rubyAdeniregun/Database/Queries.sql
@@ -1,11 +1,15 @@
 /* Question 1: Query to find the total number of passengers that survived 
 The total number of passengers who survived = 342. */
-SELECT * FROM accident_records WHERE passenger_survived LIKE 1; 
+SELECT COUNT(*)
+FROM accident_records 
+WHERE passenger_survived LIKE 1; 
 
 
 /* Question2: Query to find the total number of passengers who did not survive
 The total number of passengers who did not survive = 549. */
-SELECT * FROM accident_records WHERE passenger_survived LIKE 0; 
+SELECT COUNT(*)
+FROM accident_records 
+WHERE passenger_survived LIKE 0; 
 
 
 /* Question 3: Query to get the name and sex of passengers under 
@@ -18,16 +22,25 @@ the age of 27 that embarked at Queenstown and Cherbourg? */
   
 /* Question 4: Query to get how many of the passengers that embarked at Southampton survived? 
 Passengers that embarked at Southampton survived = 218 */
-SELECT passengers_records.registered_passengers_id
-FROM passengers_records INNER JOIN accident_records 
-ON passengers_records.registered_passengers_id = accident_records.registered_passengers_id
-WHERE point_of_embarkation LIKE 'S' AND passenger_survived LIKE 1;
+SELECT registered_passengers.id, registered_passengers.full_name
+FROM registered_passengers LEFT JOIN passengers_records 
+ON registered_passengers.id = passengers_records.registered_passengers_id
+WHERE passengers_records.trip_fare > 100 AND registered_passengers.age > '35' AND passengers_records.number_of_siblings_spouses_aboard >= 1;
 
+SELECT COUNT(registered_passengers_id)
+FROM registered_passengers LEFT JOIN passengers_records 
+ON registered_passengers.id = passengers_records.registered_passengers_id
+WHERE passengers_records.trip_fare > 100 AND registered_passengers.age > '35' AND passengers_records.number_of_siblings_spouses_aboard >= 1;
+ 
 
 /* Question 5: Query to get the id, name and the total number of passengers who paid a fare greater 
 than $100 and above the age of 35 had siblings or spouses on board.
 Total number of passengers = 9 */
-SELECT registered_passengers.id, registered_passengers.full_name, registered_passengers_id
+SELECT registered_passengers.id, registered_passengers.full_name
 FROM registered_passengers LEFT JOIN passengers_records 
 ON registered_passengers.id = passengers_records.registered_passengers_id
+WHERE passengers_records.trip_fare > 100 AND registered_passengers.age > '35' AND passengers_records.number_of_siblings_spouses_aboard >= 1
+
+SELECT COUNT(*)
+FROM registered_passengers
 WHERE passengers_records.trip_fare > 100 AND registered_passengers.age > '35' AND passengers_records.number_of_siblings_spouses_aboard >= 1;
