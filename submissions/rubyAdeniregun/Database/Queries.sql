c41cbad8bc01ac10bd3213c765fa8dfd4e2123f8
Ruby Adeniregun <rubyadeniregun@gmail.com>
2021-01-14 23:16:30 +0100

    update

--- submissions/rubyAdeniregun/Database/Queries.sql
+++ submissions/rubyAdeniregun/Database/Queries.sql
@@ -25,7 +25,7 @@ Passengers that embarked at Southampton survived = 218 */
 SELECT COUNT(passengers_records.registered_passengers_id)
 FROM passengers_records INNER JOIN accident_records 
 ON passengers_records.registered_passengers_id = accident_records.registered_passengers_id
-WHERE point_of_embarkation LIKE 'S' AND passenger_survived LIKE 1;
+WHERE point_of_embarkation = 'S' AND passenger_survived = 1;
  
 
 /* Question 5: Query to get the id, name and the total number of passengers who paid a fare greater 
@@ -34,11 +34,11 @@ Total number of passengers = 9 */
 SELECT registered_passengers.id, registered_passengers.full_name
 FROM registered_passengers LEFT JOIN passengers_records 
 ON registered_passengers.id = passengers_records.registered_passengers_id
-WHERE (passengers_records.trip_fare > 100 AND registered_passengers.age > '35') AND passengers_records.number_of_siblings_spouses_aboard >= 1;
+WHERE (passengers_records.trip_fare > 100 AND registered_passengers.age > 35) AND passengers_records.number_of_siblings_spouses_aboard >= 1;
 
 -- Total number of passengers = 9
 SELECT COUNT(registered_passengers_id)
 FROM registered_passengers LEFT JOIN passengers_records 
 ON registered_passengers.id = passengers_records.registered_passengers_id
-WHERE passengers_records.trip_fare > 100 AND registered_passengers.age > 35 AND passengers_records.number_of_siblings_spouses_aboard >= 1;
+WHERE (passengers_records.trip_fare > 100 AND registered_passengers.age > 35) AND passengers_records.number_of_siblings_spouses_aboard >= 1;
  
