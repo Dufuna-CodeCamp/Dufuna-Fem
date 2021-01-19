056c28274a14119ab629a666a358025188aaeccc
OluwaboriShobayo <oluwaboris1@gmail.com>
2021-01-19 14:51:22 +0100

    SignUpValidation

--- submissions/oluwaboriShobayo/DOM/signUp.js
+++ submissions/oluwaboriShobayo/DOM/signUp.js
@@ -6,60 +6,56 @@ let form = document.getElementById("sign");
      mail  = document.getElementById("mail");
      password = document.getElementById("password");
      password2 = document.getElementById("password2");
-
-     function required (field, event) {
-        if (firstname.value == "") {
-            firstname.nextElementSibling.innerHTML = "Please enter your firstname";
-            event.preventDefault();
-
-        if (lastname.value == "") 
-                lastname.nextElementSibling.innerHTML = "Please enter your lastname";
-                event.preventDefault();
-
-        if (phonenumber.value == "") 
-                phonenumber.nextElementSibling.innerHTML = "Please enter your phonenumber";
-                event.preventDefault();
-         if (mail.value == "") 
-                mail.nextElementSibling.innerHTML = "Please enter your Email Address";
-                event.preventDefault();
-        if (password.value == "") 
-                password.nextElementSibling.innerHTML = "Please enter your password";
-                event.preventDefault();
-         if (password2.value == "") 
-                password2.nextElementSibling.innerHTML = "Please confirm your password";
-                event.preventDefault();
-                return false;
-        } else {
-            field.nextElementSibling.innerHTML = "";
-            return true;
-        }   
-    }
-// NAME VALIDATION
-function nameValidation (event) {
+      
+// firstname VALIDATION
+function firstnameValidation (event) {
 let nameRegex = /^[A-Za-z]+$/;
-    if (!firstname.value.match(nameRegex)) {
-        firstname.nextElementSibling.innerHTML = "Special characters are not allowed";
-        event.preventDefault();
-    if (!lastname.value.match(nameRegex)) {
+if (firstname.value == '') {
+    firstname.nextElementSibling.innerHTML = "Please enter your firstname";
+    event.preventDefault()
+    return false;
+} 
+if (!firstname.value.match(nameRegex)) {
+    firstname.nextElementSibling.innerHTML = "Special characters are not allowed";
+    event.preventDefault();
+    return false;
+} else {
+    firstname.nextElementSibling.innerHTML = "";
+    return true;
+}
+}
+// Lastname validation
+function lastnameValidation (event) {
+    let nameRegex = /^[A-Za-z]+$/;
+    if (lastname.value == '') {
+    lastname.nextElementSibling.innerHTML = "Please enter your lastname";
+    event.preventDefault()
+    return false;
+} 
+  if (!lastname.value.match(nameRegex)) {
             lastname.nextElementSibling.innerHTML = "Special characters are not allowed";
             event.preventDefault();
+            return false;
     } else {
-        firstname.nextElementSibling.innerHTML = "";
-        lastname.nextElementSibling.innerHTML = "";
+      lastname.nextElementSibling.innerHTML = "";
         return true;
     }
-}
 }
 
 //  Phone Number Validation
  function numberValidation(event) {
-     let numRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3} )[-. ]?([0-9]{4})$/;
+    if (phonenumber.value == '') {
+        phonenumber.nextElementSibling.innerHTML = "Please enter your phone number";
+        event.preventDefault()
+        return false;
+    } 
+    let numRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3} )[-. ]?([0-9]{4})$/;
      if (!phonenumber.value.match(numRegex)) {
          phonenumber.nextElementSibling.innerHTML = "Please enter a valid Number";
          event.preventDefault();
          return false
-         
-     } else {
+     }   
+     else {
          phonenumber.nextElementSibling.innerHTML = "";
          return true;
      } 
@@ -67,7 +63,12 @@ let nameRegex = /^[A-Za-z]+$/;
 
      //  Email field Validation
      function emailFieldValidation(event) {
-         let emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+        if (mail.value == '') {
+            mail.nextElementSibling.innerHTML = "Please enter your email Address";
+            event.preventDefault()
+            return false;
+        }   
+        let emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
          if (!mail.value.match(emailRegex)) {
          mail.nextElementSibling.innerHTML = "Please enter a valid email";
          event.preventDefault();
@@ -81,12 +82,16 @@ let nameRegex = /^[A-Za-z]+$/;
 
      // PasswordValidation
 function passwordValidation(event) {
+    if (password.value == '') {
+        password.nextElementSibling.innerHTML = "Please enter your password";
+        event.preventDefault()
+        return false;
+    } 
     let pwdRegex = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/;
     if (!password.value.match(pwdRegex)) {
         password.nextElementSibling.innerHTML = "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character eg @ and 1 number";
         event.preventDefault();
         return false;
-      
     }  
         else if (!(password.value.length >= 6)) {
         password.nextElementSibling.innerHTML = "Your password length must have a minimum of 6 characters";
@@ -101,6 +106,11 @@ function passwordValidation(event) {
 }
  // Confirm password
 function password2Validation(event) {
+    if (password2.value == '') {
+        password2.nextElementSibling.innerHTML = "Please confirm your password";
+        event.preventDefault()
+        return false;
+    } 
     if (password2 !== password) {
         password2.nextElementSibling.innerHTML = "Your passwords don't match"
         event.preventDefault();
@@ -112,14 +122,9 @@ function password2Validation(event) {
 }
 
     function forms(e) {
-        required (firstname, e);
-        required (lastname, e);
-        required (phonenumber, e);
-        required (mail, e);
-        required (password, e);
-        required (password2, e);
         emailFieldValidation(e);
-        nameValidation(e);
+        firstnameValidation(e);
+        lastnameValidation(e);
         numberValidation(e);
         passwordValidation(e);
         password2Validation(e);
