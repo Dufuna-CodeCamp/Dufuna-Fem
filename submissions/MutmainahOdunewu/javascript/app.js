0dc8c5a26b994be20518cc56acc60fd0794ffa9d
odunewu mutmainah <odunewumainah@gmail.com>
2021-01-20 19:18:56 +0100

    updated javascript

---
+++ submissions/MutmainahOdunewu/javascript/app.js
@@ -0,0 +1,12 @@
+// 7-123
+for (let i = 7; i <= 123; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("software Developer");
+  } else if (i % 3 === 0) {
+    console.log("softwear");
+  } else if (i % 5 === 0) {
+    console.log("Developer");
+  } else {
+    console.log(i);
+  }
+}
