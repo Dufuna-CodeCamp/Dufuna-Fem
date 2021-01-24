5ec2446c289f1b97a265bb0e9d93e2d11a483995
OluwaboriShobayo <oluwaboris1@gmail.com>
2021-01-24 18:28:42 +0100

    Modified

--- submissions/oluwaboriShobayo/DOM/contactusValidation.js
+++ submissions/oluwaboriShobayo/DOM/contactusValidation.js
@@ -3,66 +3,47 @@ let formValidation = document.getElementById('form');
      email = document.getElementById("mail");
      subject = document.getElementById("subject");
      message = document.getElementById("message");
+     nameRegex = /^[A-Za-z]+$/;
+     emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
 
-    function nameValidation (event) {
+    function emptyField (event) {
         if (fName.value == '') {
             fName.nextElementSibling.innerHTML = "Please enter your firstname";
             event.preventDefault()
+           
+        }   if (subject.value == '') {
+            subject.nextElementSibling.innerHTML = "Please enter a message subject";
+            event.preventDefault();
+           
+          } 
+          if (email.value == '') {
+            email.nextElementSibling.innerHTML = "Please enter your email Address";
+            event.preventDefault()
+           
+        }   
+            if (message.value == '') {
+            message.nextElementSibling.innerHTML = "Please enter your Message";
+            event.preventDefault()
             return false;
-        } 
-        let nameRegex = /^[A-Za-z]+$/;
+        } else {
+            field.nextElementSibling.innerHTML = "";
+            return true;
+        }
+    }
+     function nameValidation (event) {
         if (!fName.value.match(nameRegex)) {
             fName.nextElementSibling.innerHTML = "Special characters are not allowed";
-            event.preventDefault();
-            return false ;
+            event.preventDefault()
+        }
+         if (!email.value.match(emailRegex)) {
+            email.nextElementSibling.innerHTML = "Please enter a valid email";
+            event.preventDefault()
+            return false;
         } else {
-            fName.nextElementSibling.innerHTML = "";
+            field.nextElementSibling.innerHTML = "";
             return true;
         }
     }
 
-    function emailValidation (event) {
-        if (email.value == '') {
-            email.nextElementSibling.innerHTML = "Please enter your email Address";
-            event.preventDefault()
-            return false;
-        }   
-        let emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
-         if (!email.value.match(emailRegex)) {
-         email.nextElementSibling.innerHTML = "Please enter a valid email";
-         event.preventDefault();
-         return false;
-         } else  {
-         email.nextElementSibling.innerHTML = "";
-         return true;
-     }
-     } 
-     
-     function subjectValidation (event) {
-        if (subject.value == '') {
-        subject.nextElementSibling.innerHTML = "Please enter a message subject";
-        event.preventDefault();
-        return false;
-      } 
-      if (subject.nextElementSibling.innerHTML = "");
-        return true;
-     }
-
-     function messageValidation (event) {
-        if (message.value == '') {
-        message.nextElementSibling.innerHTML = "Please enter your Message";
-        event.preventDefault()
-        return false;
-    } 
-    if (message.nextElementSibling.innerHTML = "");
-        return true;
-}
-    
-     function validateForm(e) {
-         nameValidation(e);
-         emailValidation(e);
-         subjectValidation(e);
-         messageValidation(e);
-         return true;
-    }
-     formValidation.addEventListener("submit", validateForm);
+     formValidation.addEventListener("submit", nameValidation);
+     formValidation.addEventListener("submit", emptyField);
