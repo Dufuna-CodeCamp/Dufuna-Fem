dbad7ee87053794aa450fac071fd06a410385829
Ruby Adeniregun <rubyadeniregun@gmail.com>
2021-01-29 22:32:53 +0100

    arraystask

---
+++ submissions/rubyAdeniregun/php/arraystask.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$firstname = ['Chika', 'Destiny', 'Drake', 'Steve', 'Leonard', 'Mark', 'Justus', 'Daniel'];
+
+$hobby = ['Swimming', 'Volleyball', 'Reading', 'Cooking', 'Football', 'Travelling', 'Singing', 'Jogging'];
+
+
+foreach ($firstname as $key => $value) {
+  
+        echo "My name is $firstname[$key]. I love $hobby[$key] <br><br>";
+
+};
+
+?>
