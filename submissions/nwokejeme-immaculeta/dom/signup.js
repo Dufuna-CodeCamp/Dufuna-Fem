d4cd686c7d7ffa35c4c82a070444dc666759f3b5
Nwokejeme Immaculeta <immaculetapascaline@gmail.com>
2021-02-03 22:20:43 +0100

    made some changes

--- submissions/nwokejeme-immaculeta/dom/signup.js
+++ submissions/nwokejeme-immaculeta/dom/signup.js
@@ -34,23 +34,22 @@ let passwordValidation = (
         confirmPassword.nextElementSibling.innerHTML = '';
         return true;
     } 
- 
-    formValidation =(event) => {
-        const nameRegex = /^[a-zA-Z]/,
-          phoneRegex = /^[\+]?[0-9]{8,14}$/i,
-          emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/,
-          passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{6,}$/;
-          
-          validate(firstName, event, nameRegex, "Please enter your First Name", "Please enter a valid First Name");
-          validate(lastName, event, nameRegex, "Please enter your Last Name", "Please enter a valid Last Name");
-          validate(phoneNumber, event, phoneRegex, "Please enter your Phone Number", "Please enter a valid Phone Number");
-          validate(email, event, emailRegex, "Please enter your Email", "Please enter a valid Email");
-          validate(password, event, passwordRegex, 'Please enter your password','Your password should have a minimum of 6 characters, 1 capital letter, 1 special character eg @ and 1 number');
-          validate(confirmPassword, event, passwordRegex, 'Please confirm your Password', 'Your password do not match')
-          passwordValidation(confirmPassword, password);
-    }  
     
-}       
+}    
 
+formValidation =(event) => {
+    const nameRegex = /^[a-zA-Z]/,
+      phoneRegex = /^[\+]?[0-9]{8,14}$/i,
+      emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/,
+      passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{6,}$/;
+      
+      validate(firstName, event, nameRegex, "Please enter your First Name", "Please enter a valid First Name");
+      validate(lastName, event, nameRegex, "Please enter your Last Name", "Please enter a valid Last Name");
+      validate(phoneNumber, event, phoneRegex, "Please enter your Phone Number", "Please enter a valid Phone Number");
+      validate(email, event, emailRegex, "Please enter your Email", "Please enter a valid Email");
+      validate(password, event, passwordRegex, 'Please enter your password','Your password should have a minimum of 6 characters, 1 capital letter, 1 special character eg @ and 1 number');
+      validate(confirmPassword, event, passwordRegex, 'Please confirm your Password', 'Your password do not match')
+      passwordValidation(confirmPassword, password);
+}  
 submitForm.addEventListener("submit", formValidation);
     
