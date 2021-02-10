4a1c06d9ade37170a9b4f07547ca32c228544fc2
Ruby Adeniregun <rubyadeniregun@gmail.com>
2021-02-10 02:18:00 +0100

    update

--- submissions/rubyAdeniregun/php/customer_cookie.php
+++ submissions/rubyAdeniregun/php/customer_cookie.php
@@ -1,6 +1,14 @@
 <?php
-    setcookie("name", "John Mark", time()+3600, "/", "", false);
-    setcookie("email", "johnmark@mail.com", time()+3600, "/", "", false);
+    setcookie("serial_number", "", time()+ 3600, "/", "", 0);
+
+    setcookie("full_name", "", time()+ 3600, "/", "", 0);
+    
+    setcookie("email_address", "", time()+ 3600, "/", "", 0 );
+    
+    setcookie("created_at", "", time()+ 3600, "/", "", 0 );
+    
+    setcookie("actions", "", time()+ 3600, "/", "", 0 );
+    
 ?>
 
 <html>
@@ -11,6 +19,6 @@
         <?php echo "<h1>Set Cookies"."</h1><br/>"; ?>
         <h2>My name is <?php echo $_COOKIE["name"]. "<br/>"; ?></p>
         <h2>my email address is <?php echo $_COOKIE["email"]; ?> </p>
-        <a href="customer_cookie2.php"><h5>Customer list View  page</h5></a>
+        <a href="listview.php"><h5>Customer list View  page</h5></a>
     </body>
 </html>
---
+++ submissions/rubyAdeniregun/php/listview.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("db_connection.php");
+
+echo "<h5> List View Page </h5>";
+
+try{
+    $sql= "SELECT * FROM customer_details";
+    $result= $pdo->query($sql);
+    if ($result->rowCount() > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>serial_number</th>";
+                echo "<th>full_name</th>";
+                echo "<th>email_address</th>";
+                echo "<th>created_at</th>";
+                echo "<th>actions</th>";
+            echo "</tr>";
+        while($row = $result->fetch()){
+            echo "<tr>";
+                echo "<td>" . $row['serial_number'] . "</td>";
+                echo "<td>" . $row['full_name'] . "</td>";
+                echo "<td>" . $row['email_address'] . "</td>";
+                echo "<td>" . $row['created_at'] . "</td>";
+                echo "<td>" . $row['actions'] . "</td>";
+                echo "<td><button type=button>View</button>" . $row['actions'] . "</td>";
+            echo "</tr>";
+    }
+        echo "</table>";
+        unset($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} catch(PDOException $e){
+    die("ERROR:Could not be able to execute $sql. " . $e->getMessage());
+}
+
+unset($pdo);
