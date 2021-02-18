6af45308f6fb113fb3d3789e0fd73fa7621e8f02
damlois <lois0168@gmail.com>
2021-02-18 12:07:35 +0100

    updted PR

--- submissions/loisAdegbohungbe/DOM/newsletter.js
+++ submissions/loisAdegbohungbe/DOM/newsletter.js
@@ -1,18 +1,18 @@
-let  newsForm = document.getElementById('newsletter-form');
-let  newsEmail = document.getElementById('newsEmail');
+let newsForm = document.getElementById('newsletter-form');
+let newsEmail = document.getElementById('newsEmail');
 
 validateForm = (event) => {
     let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/;
     if (newsEmail.value == '') {
-        newsEmail.previousElementSibling.innerHTML = "Please enter your Email Address";
+        newsForm.lastElementChild.innerHTML = "Please enter your Email Address";
         event.preventDefault();
     }
     else if (!newsEmail.value.match(emailRegex)) {
-        newsEmail.previousElementSibling.innerHTML = "please enter a valid email";
+        newsForm.lastElementChild.innerHTML = "please enter a valid email";
         event.preventDefault();
     }
     else {
-        newsEmail.previousElementSibling.innerHTML = "";
+        newsForm.lastElementChild.innerHTML = "";
         return true;
     }
 }
