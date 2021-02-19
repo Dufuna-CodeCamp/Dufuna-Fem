2827ee8f50cb50c3cc0a256eaa4496943866f419
Nwokejeme Immaculeta <immaculetapascaline@gmail.com>
2021-02-19 12:25:45 +0100

    made changes

--- submissions/nwokejeme-immaculeta/database/init.sql
+++ submissions/nwokejeme-immaculeta/database/init.sql
@@ -13,7 +13,7 @@ CREATE TABLE admins (
 );
 
 // INSERT RECORD INTO ADMINS TABLE
-INSERT INTO admins (name, email, password, phone_number,)
+INSERT INTO admins (name, email, password, phone_number)
 VALUES  ('Hannah Donald', 'hannahdonald@gmail', 'july31', '+234808437228'),
 	('Michael Jackson', 'mikejacson@yahoo.com', 'micson3', '+23490893144879'),
 	('Donald Trump ', 'donaldtrump@sample.com', 'trump4life', '+2344903002'),	
@@ -27,15 +27,15 @@ CREATE TABLE categories (
 
 // INSERT DATA INTO CATEGORIES TABLE
 INSERT INTO categories (category_name)
-VALUES 	('Bag')
+VALUES 	('Bag'),
 	('Belt'),
 	('Wristwatch'),
-    ('sunglasses'),
+    ('sunglasses');
 					                   
 // CREATE PRODUCTS TABLE
 CREATE TABLE products (
     product_id INT NOT NULL AUTO_INCREMENT,
-	categories_id INT NOT NULL,
+	category_id INT NOT NULL,
 	admins_id INT NOT NULL,
 	product_name VARCHAR(50) NOT NULL,
 	product_description VARCHAR(250) NOT NULL,
@@ -43,15 +43,15 @@ CREATE TABLE products (
 	unit_price DECIMAL (10,2) NOT NULL,
     stock_level VARCHAR(50) NOT NULL,
     product_status VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id),
-	FOREIGN KEY (categories_id) REFERENCES categories (id),
-	FOREIGN KEY (admins_id) REFERENCES admins (id)
+    PRIMARY KEY (product_id),
+	FOREIGN KEY (category_id) REFERENCES categories (category_id),
+	FOREIGN KEY (admins_id) REFERENCES admins (admin_id)
 );
 
  // INSERT INTO PRODUCTS TABLE
-INSERT INTO products (category_id, admin_id, product_name, product_description, product_image, unit_price, stock_level, product_status)
-VALUES 	(1,2,'Belt', 'A classique leather for men and women', 'C:\Users\kajoe\Desktop\Dufuna-Fem\submissions\nwokejeme-immaculeta\html\Image\sku_brown_1_10.jpg'),8000, 6, 'in stock'),
-    (1,1,'Wristwatch', 'A gold-plaited wristwatch for classique men and women', 'C:\Users\kajoe\Desktop\Dufuna-Fem\submissions\nwokejeme-immaculeta\html\Image\images1.jpg' ,6000, 3, 'in stock'),
+INSERT INTO products (category_id, admins_id, product_name, product_description, product_image, unit_price, stock_level, product_status)
+VALUES 	(1,2,'Belt', 'A classique leather for men and women', 'C:\Users\kajoe\Desktop\Dufuna-Fem\submissions\nwokejeme-immaculeta\html\Image\sku_brown_1_10.jpg', 8000, 6, 'in stock'),
+    (1,1,'Wristwatch', 'A gold-plaited wristwatch for classique men and women', 'C:\Users\kajoe\Desktop\Dufuna-Fem\submissions\nwokejeme-immaculeta\html\Image\images1.jpg', 6000, 3, 'in stock');
 
 // CREATE CUSTOMERS TABLE
 CREATE TABLE customers (
@@ -72,7 +72,7 @@ INSERT INTO customers (first_name, last_name, email, password)
 
 // CREATE CUSTOMERS ADDRESS TABLE 
 CREATE TABLE customer_address (
-	product_id INT NOT NULL AUTO_INCREMENT,
+	address_id INT NOT NULL AUTO_INCREMENT,
  	street VARCHAR(50) NOT NULL, 
  	city VARCHAR(40) NOT NULL, 
  	state VARCHAR(40) NOT NULL, 
@@ -80,48 +80,48 @@ CREATE TABLE customer_address (
  	country VARCHAR(30) NOT NULL,
  	phone_number VARCHAR(20) NOT NULL,
  	customer_id INT NOT NULL,
- 	PRIMARY KEY(id),
- 	FOREIGN KEY (customer_id) REFERENCES customer (id)
+ 	PRIMARY KEY(address_id),
+ 	FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
 );
 
 // INSERT INTO CUSTOMERS ADDRESS TABLE
-INSERT INTO customer_contacts (customer_id, street, city, state, zip_code, country, phone)
+INSERT INTO customer_address (customer_id, street, city, state, zip_code, country, phone_number)
 VALUES  (1, '45 Rasaki oseni Street', 'Adebayo', 'Ekiti', 23444 , 'Nigeria', '0803508168'),
 		(2, '45 King and Queen', 'Kubua', 'Abuja', 557654 , 'Nigeria', '08163243832'),
 		(3, '12 A close first avenue', 'Festac', 'lagos', 432185 , 'Nigeria', '07033352641');
                     
 // CREATE ORDER TABLE
 CREATE TABLE orders (
-	product_id INT NOT NULL AUTO_INCREMENT,
-	amount DECIMAL(10,2) NOT NULL,
-	date DATETIME NOT NULL,
+	order_id INT NOT NULL AUTO_INCREMENT,
+	order_amount DECIMAL(10,2) NOT NULL,
+	order_created_at DATETIME,
 	customer_id INT NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY (customer_id) REFERENCES customer (id)
+	PRIMARY KEY(order_id),
+	FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
 );
 
 // INSERT INTO ORDERS TABLE
-INSERT INTO orders (amount, date, customer_id)
-VALUES(6000 now(), 1),
+INSERT INTO orders (order_amount, order_created_at, customer_id)
+VALUES(6000, now(), 1),
 	(8000, now(), 2),
-	(6000 now(), 3);
+	(6000, now(), 3);
 
 
 // CREATE ORDER ITEMS 
 CREATE TABLE order_items (
-	product_id INT NOT NULL AUTO_INCREMENT,
+	item_id INT NOT NULL AUTO_INCREMENT,
+	order_id INT NOT NULL, 
+	product_id INT NOT NULL,
 	quantity INT NOT NULL,
 	unit_prices INT NOT NULL,
 	total_amount DECIMAL(10,2) NOT NULL,
-	order_id INT NOT NULL,
-	customer_id INT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (order_id) REFERENCES orders (id),
-	FOREIGN KEY (product_id) REFERENCES products (id)
+	PRIMARY KEY (item_id),
+	FOREIGN KEY (order_id) REFERENCES orders (order_id),
+	FOREIGN KEY (product_id) REFERENCES products (prduct_id)
 );
 
 //INSERT INTO ORDER ITEMS
-INSERT INTO order_items(quantity, unit_prices, total_amount, order_id, product_id)
-VALUES (3, 6000, 18000, 1, 1),
+INSERT INTO order_items(quantity, unit_prices, total_amount, order_id, item_id)
+VALUES (3, 6000, 18000, 1, 3),
 	(2, 8000, 160000, 2, 2),
-	(1, 6000, 6000, 1, 1); 
+	(1, 6000, 6000, 3, 1); 
