bd9b8495e47504c2fd43b2f2cb2dd51bf25a795d
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-02-25 13:29:25 +0100

    add arrays task

---
+++ submissions/motunrayoIlawole/php/arrays.php
@@ -0,0 +1,10 @@
+<?php
+
+$firstNames = ["Naruto", "Sakura", "Hibari", "Kageyama", "Kazahaya", "Neji", "Roy", "Sasuke"];
+
+$hobbies = ["Fighting", "Training", "Sleeping", "Playing volleyball", "Writing", "Hiking", "Singing", "Flying"];
+
+foreach($firstNames as $key => $value) {
+
+    echo "My name is $firstNames[$key]. I love $hobbies[$key]. <br>";
+};
