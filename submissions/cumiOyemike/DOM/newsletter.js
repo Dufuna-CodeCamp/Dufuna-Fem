f8daf2a00d2217f6c242ea8931d2010b0ad086a5
ctripleu <cuoyemike@gmail.com>
2021-03-11 10:58:53 +0100

    made a correction

--- submissions/cumiOyemike/DOM/newsletter.js
+++ submissions/cumiOyemike/DOM/newsletter.js
@@ -1,26 +1,26 @@
-const domElements2 = {
+const newsletterDom = {
     footerBtn: document.querySelector('.footer-btn'),
     footerEmail: document.getElementById('footer-email'),
     errorBlock: document.querySelector('.error-block2')
 }
 
 const validateNewsletter = (e) => {
-    let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (domElements2.footerEmail.value === '') {
-        domElements2.errorBlock.innerHTML = "Please enter your email address";
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (newsletterDom.footerEmail.value === '') {
+        newsletterDom.errorBlock.innerHTML = "Please enter your email address";
         e.preventDefault();
         return false;
-    } else if (!reg.test(domElements2.footerEmail.value.trim()) ) {
-        domElements2.errorBlock.innerHTML = "Please enter a valid email";
+    } else if (!reg.test(newsletterDom.footerEmail.value.trim()) ) {
+        newsletterDom.errorBlock.innerHTML = "Please enter a valid email";
         e.preventDefault();
         return false;
     } else {
-        domElements2.errorBlock.innerHTML = "";
+        newsletterDom.errorBlock.innerHTML = "";
         return true;
     }
 }
 
-domElements2.footerBtn.addEventListener('click', validateNewsletter);
+newsletterDom.footerBtn.addEventListener('click', validateNewsletter);
 
 
 /*let footerEmail = document.getElementById('footer-email'),
