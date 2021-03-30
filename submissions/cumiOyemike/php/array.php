6c7627e80f02359d59a53d1050111f4b7294e6d2
ctripleu <cuoyemike@gmail.com>
2021-03-30 13:41:30 +0100

    pushed in error

--- submissions/cumiOyemike/php/array.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$firstName = ['Chidi', 'Aisha', 'Dami', 'Kosi', 'Diepiriye', 'Sam', 'Osayuwa', 'Isioma'];
-
-$hobby =['Hiking', 'Singing', 'Golfing', 'Running', 'Painting', 'Sculpting', 'Playing the ukulele', 'Making videos'];
-
-foreach ($firstName as $key => $value) { 
-    echo "My name is $firstName[$key]. I love $hobby[$key].<br>";
-}
