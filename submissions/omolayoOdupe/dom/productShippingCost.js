29d1672e755dcff7c4c1b9c166683ebaee46b539
Omolayo O <mollyqueen_o@yahoo.com>
2021-04-07 00:53:30 +0100

    product shipping cost updated

--- submissions/omolayoOdupe/dom/productShippingCost.js
+++ submissions/omolayoOdupe/dom/productShippingCost.js
@@ -2,23 +2,32 @@ let quantity = document.getElementById('quantity'),
     price = document.getElementById('price'),
     shippingCost = document.getElementById('shippingCost'),
     totalCost = document.getElementById('totalCost'),
-    button = document.getElementById('buyNow');
+    button = document.getElementById('buyNow'),
     productPrice = Number(price.innerHTML) * quantity.value;
     totalCost.innerHTML = Number(shippingCost.innerHTML) + productPrice;
 
-function productPriceCheck (event) {
+function quantityCheck (event) {
     if (quantity.value <= 0){
         quantity.nextElementSibling.innerHTML = 'please enter a valid quantity';
         button.disabled = true;
-        button.classList.add('disable')
+        button.style = 'background-color: grey';
         event.preventDefault();
         return false;
     } else {
         productPrice = price.innerHTML * quantity.value
         quantity.nextElementSibling.innerHTML = '';
         button.disabled = false;
-        button.classList.remove('disable')
         return true;
+    }
+}
+
+function productPriceCheck (event) {
+    if (quantity.value >= 1){
+        price.innerHTML = productPrice;
+    } else if (quantity.value ===''){
+        price.innerHTML = 25000;
+    }else {
+        return false;
     }
 }
 
@@ -35,9 +44,19 @@ function shippingPriceCheck (event){
         }
 }
 
+function totalCostCheck (event){
+    if (quantity.value >= 1){
+        totalCost.innerHTML = Number(shippingCost.innerHTML) + productPrice;
+    }else {
+        return false;
+    }
+}
+
 function productPageValidation (e){
-    productPriceCheck (e);
+    quantityCheck (e);
     shippingPriceCheck(e);
+    productPriceCheck(e);
+    totalCostCheck(e);
     return true
 }
 
