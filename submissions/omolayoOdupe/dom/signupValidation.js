b6b546bf74d77fc52abca78a484e3b65e9a98c77
Omolayo O <mollyqueen_o@yahoo.com>
2021-04-10 11:07:49 +0100

    regExp added to inputCheck parameter

--- submissions/omolayoOdupe/dom/signupValidation.js
+++ submissions/omolayoOdupe/dom/signupValidation.js
@@ -1,4 +1,4 @@
-let submitForm = document.getElementById('form');
+let form = document.getElementById('form');
 let firstName = document.getElementById('firstname');
 let middleName = document.getElementById('middlename');
 let lastName = document.getElementById('lastname');
@@ -7,25 +7,17 @@ let email = document.getElementById('email');
 let password = document.getElementById('password');
 let confirmPassword = document.getElementById('password2');
 
-function inputCheck (field, event){
+function inputCheck (field, regExp, error_block1, error_block2, event){
     if (field.value == ''){
-        field.nextElementSibling.innerHTML == 'error-block1';
+        field.nextElementSibling.innerHTML = error_block1;
         event.preventDefault();
         return false;
     } else if (!field.value.match(regExp)){
-        field.nextElementSibling.innerHTML == 'error-block2';
-        event.preventDefault();
-        return false;
-    } else if (!(field.value.lenght <= 6)){
-        field.nextElementSibling.innerHTML == 'error-block3';
-        event.preventDefault();
-        return false;
-    } else if (field.value != field.value){
-        field.nextElementSibling.innerHTML == 'error-block4';
+        field.nextElementSibling.innerHTML = error_block2;
         event.preventDefault();
         return false;
     } else {
-        field.nextElementSibling.innerHTML == '';
+        field.nextElementSibling.innerHTML = '';
         return true;
     }
 }
@@ -35,70 +27,14 @@ function inputValidation (event){
     let numberRegex = /^[\+]?[0-9]{8,14}$/;
     let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/;
     let pwdRegex = /(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{6,}$/;
-    
-    if (firstName.value ==''){
-        firstName.nextElementSibling.innerHTML = 'please enter your first name';
-        event.preventDefault();
-
-    if (lastName.value == '')
-        lastName.nextElementSibling.innerHTML = "Please enter your Last Name";
-        event.preventDefault();
-
-    if (phoneNo.value == '')
-        phoneNo.nextElementSibling.innerHTML = "Please enter your phone number";
-        event.preventDefault();
-
-    if (email.value == '')
-        email.nextElementSibling.innerHTML = "Please enter your email address";
-        event.preventDefault();
-
-    if (password.value == '')
-        password.nextElementSibling.innerHTML = "Please enter your password";
-        event.preventDefault();
-
-    if (confirmPassword.value == '')
-        confirmPassword.nextElementSibling.innerHTML = "Please confirm your password";
-        event.preventDefault();
-        return false;
-        
-    } else if (!firstName.value.match(nameRegex)){
-        firstName.nextElementSibling.innerHTML = 'please enter a valid first name';
-        event.preventDefault();
-
-        (!lastName.value.match(nameRegex))
-        lastName.nextElementSibling.innerHTML = 'please enter a valid last name';
-        event.preventDefault();
-
-        (!phoneNo.value.match(numberRegex))
-        phoneNo.nextElementSibling.innerHTML = 'please enter a valid phone number';
-        event.preventDefault();
-
-        (!email.value.match(emailRegex))
-        email.nextElementSibling.innerHTML = 'please enter a valid email address';
-        event.preventDefault();
-
-        (!password.value.match(pwdRegex))
-        password.nextElementSibling.innerHTML = 'your password should have a minimum 6 character, 1 capital letter, 1 special character e.g @ and 1 number';
-        event.preventDefault();
-        return false;
-
-    } else if (!(password.value.lenght <= 6)){
-        password.nextElementSibling.innerHTML = 'password lenght must be minimum of 6 character';
-        event.preventDefault();
-        return false;
-
-    } else if (confirmPassword.value != password.value){
-        confirmPassword.nextElementSibling.innerHTML = 'password do not match';
-        event.preventDefault();
-        return false;
-
-    } else {
-        field.nextElementSibling.innerHTML = '';
-        return true;
-    }
-
+    inputCheck (firstName, nameRegex, 'please enter your first name', 'please enter a valid first name', event);
+    inputCheck (lastName, nameRegex, 'please enter your last name', 'please enter a valid last name', event);
+    inputCheck (phoneNo, numberRegex, 'please enter your phone number', 'please enter a valid phone number', event);
+    inputCheck (email, emailRegex, 'please enter your email address', 'please enter a valid email address', event);
+    inputCheck (password, pwdRegex, 'please enter your password', 'password should have a minimum of 6 characters, 1 capital letter, 1 special character eg @ and 1 number', event);
+    inputCheck (confirmPassword, pwdRegex, 'please confirm your password', 'your password do no match', event);  
+    return true;  
 }
-
-submitForm.addEventListener('submit', inputValidation);
+form.addEventListener('submit', inputValidation);
 
 
