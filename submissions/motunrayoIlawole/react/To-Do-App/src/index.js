32efcab358d98a954183a6eefcf16c0cc4c73b3c
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-04-14 02:04:56 +0100

    add task array

--- submissions/motunrayoIlawole/react/To-Do-App/src/index.js
+++ submissions/motunrayoIlawole/react/To-Do-App/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const TASKS = [
+	{ id: "task-0", name: "Eat", status: "active" },
+	{ id: "task-1", name: "Sleep", status: "active" },
+	{ id: "task-2", name: "Go Shopping", status: "active" },
+	{ id: "task-3", name: "Study", status: "active" },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks = {TASKS} />
   </React.StrictMode>,
   document.getElementById('root')
 );
