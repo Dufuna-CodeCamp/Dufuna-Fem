b9cb234e3024f67c31891b8331ef6681b5d034d3
IKPONMWOSA OSAGIE <arinolaosagie@gmail.com>
2021-04-26 10:14:06 +0100

    developerConditionals

---
+++ submissions/arinolaOsagie/javascript/developerConditionals.js
@@ -0,0 +1,17 @@
+productList.forEach(function(product) {
+    console.log(product.productName + " with " + product.imageURL + " is sold for " + product.productPrice)
+})
+
+conditionals task
+var a = 123 - 7;
+for (var i = 1; i <= a; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("Software Developer");
+    } else if (i % 3 === 0) {
+        console.log("Software");
+    } else if (i % 5 === 0) {
+        console.log("Developer");
+    } else {
+        console.log(i)
+    }
+}
