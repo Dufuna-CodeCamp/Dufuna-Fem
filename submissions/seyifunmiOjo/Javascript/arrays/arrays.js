6b06dfd6348e35f4401acbb7d2ef0fe094c98153
Seyifunmi Ojo <seyifunmiojo11@gmail.com>
2021-05-14 01:26:20 +0100

    re-did the task

--- submissions/seyifunmiOjo/Javascript/arrays/arrays.js
+++ submissions/seyifunmiOjo/Javascript/arrays/arrays.js
@@ -1,9 +1,23 @@
-let productList = {
-    productName: "lemons",
-    imageUrl: "images/lemons.jpg", 
-    productPrice: "$5"};
+const productList = [
+        {
+    productName: "Apples",
+    imageUrl: "images/apples.jpg", 
+    productPrice: "$5"},
+    {
+        productName: "Strawberries",
+        imageUrl: "images/strawberries.jpg",
+        productPrice: "$8"
+    },
+    {
+        productName:"Pawpaws",
+        imageUrl: "images/pawpaws.jpg",
+        productPrice: "$10"
+    },
+    ];
 
+    productList.forEach((productList) => { 
 console.log (productList.productName + " " + "with" +" " + productList.imageUrl + " " + "is sold for" + " " + productList.productPrice);
+    });
     
 
     
