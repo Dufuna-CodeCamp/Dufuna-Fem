06807c0955958fd4937d2c052fc51816e56144da
OmolayoO <75140375+OmolayoO@users.noreply.github.com>
2021-05-16 11:19:59 +0100

    Delete index.js

--- submissions/omolayoOdupe/javaScript/index.js
+++
@@ -1,9 +0,0 @@
-let productList = [
-    {productName: 'Wine and Confectionery Basket' , imageUrl: 'wineHamper.jpg' , productPrice:'₦ 30,000'},
-    {productName:'Fruit Basket', imageUrl:'fruitHamper2.jpg' , productPrice:'₦ 15,000'},
-    {productName:'Corporate Basket' , imageUrl: 'coporateGift2.jpg' , productPrice:'₦ 20,000'}
-]
-
-productList.forEach ((product) => {
-        console.log ( product.productName + " " +'with' + " " + product.imageUrl + " " +'is sold for' +" " + product.productPrice);
-    })
