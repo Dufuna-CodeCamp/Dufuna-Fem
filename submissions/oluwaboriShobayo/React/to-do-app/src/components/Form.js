7e2f19378da797276a818bfabc8a2f962f57d3f6
Oluwabori <oluwaboris1@yahoo.com>
2021-06-22 19:52:48 +0100

    To do list

---
+++ submissions/oluwaboriShobayo/React/to-do-app/src/components/Form.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Form = ({setInputText, todos, setTodos,  inputText }) => {
+
+    // Function that updates the input field
+    const inputFieldHandler =(e) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+ const submitHandler = (e) => {
+        e.preventDefault();
+        setTodos ([
+                ...todos, {text:inputText, completed:false, id:Math.random()}
+        ]);
+        setInputText('');
+    };
+    return (
+            <form className="InputTodo">
+                <input 
+                    value={inputText} 
+                    onChange={inputFieldHandler} 
+                    type="text" 
+                    className="TodoInput" 
+                    placeholder="Create a new todo..."
+                />
+                <button
+                    onClick={submitHandler} 
+                    className="submitInput" 
+                    type="submit"
+                  >
+                    <i className="fa fa-plus"></i>
+                </button>
+            </form>
+    );
+};
+
+
+export default Form;
---
+++ submissions/oluwaboriShobayo/React/to-do-app/src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Todo from './Todo';
+
+const Todolist = ({todos, setTodos }) => {
+    console.log(todos);
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                {todos.map(todo => (
+                    <Todo 
+                    setTodos={setTodos}
+                    todos={todos}
+                    key={todo.id}
+                    todo={todo}
+                    text={todo.text}
+                      />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Todolist;
